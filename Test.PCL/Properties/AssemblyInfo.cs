9dca06c24f75436925eba4b85fe63d8b822ff2c7
Miguel Santos Silva <miguel@Miguels-MacBook-Air.local>
2018-03-18 15:45:03 +0000

    Renamed StencilORM to StencilORM.PCL

--- Test.PCL/Properties/AssemblyInfo.cs
+++ Test.PCL/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Test.PCL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
